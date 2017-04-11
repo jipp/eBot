8893319ce4367d320aa0ed64b57ca669f51a90eb
jipp <wolfgang.keller@wobilix.de>
2017-04-11 15:06:57 +0200

    init

---
+++ src/EBot.cpp
@@ -0,0 +1,97 @@
+#include "Arduino.h"
+#include <EBot.h>
+
+Servo servo;
+
+EBot::EBot() {
+  pinMode(IN1, OUTPUT);
+  pinMode(IN2, OUTPUT);
+  pinMode(IN3, OUTPUT);
+  pinMode(IN4, OUTPUT);
+  pinMode(ENA, OUTPUT);
+  pinMode(ENB, OUTPUT);
+  pinMode(Echo, INPUT);
+  pinMode(Trig, OUTPUT);
+  servo.attach(ServoPin);
+}
+
+EBot::~EBot() {
+  servo.detach();
+}
+
+void EBot::move(bool speed) {
+  digitalWrite(ENA, speed);
+  digitalWrite(ENB, speed);
+}
+
+void EBot::move(int speed) {
+  analogWrite(ENA, speed);
+  analogWrite(ENB, speed);
+}
+
+void EBot::stop() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, LOW);
+}
+
+void EBot::forward() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+void EBot::backward() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void EBot::turnLeft() {
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+}
+
+void EBot::turnRight() {
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+  digitalWrite(IN3, LOW);
+  digitalWrite(IN4, HIGH);
+}
+
+float EBot::distance() {
+  float distance = 0.0;
+
+  digitalWrite(Trig, LOW);
+  delayMicroseconds(20);
+  digitalWrite(Trig, HIGH);
+  distance = pulseIn(Echo, HIGH);
+
+  return distance/58;
+}
+
+void EBot::servoRotate(int angle) {
+  angle = angle < 0 ? 0 : angle;
+  angle = angle > 180 ? 180 : angle;
+
+  servo.write(angle);
+}
+
+EBot::line EBot::followLine() {
+  int ls1 = digitalRead(LightSensor1);
+  int ls2 = digitalRead(LightSensor2);
+  int ls3 = digitalRead(LightSensor3);
+
+  if (ls1 && ls2 & !ls3) {
+    return EBot::LEFT;
+  } else if (!ls1 && ls2 && ls3) {
+    return EBot::RIGHT;
+  } else {
+    return EBot::OK;
+  }
+}
---
+++ src/EBot.h
@@ -0,0 +1,42 @@
+#ifndef EBot_h
+#define EBot_h
+
+#include "Arduino.h"
+#include <Servo.h>
+
+#define ENA 5
+#define ENB 11
+#define IN1 6
+#define IN2 7
+#define IN3 8
+#define IN4 9
+#define Echo  A4
+#define Trig  A5
+#define ServoPin  3
+#define LightSensor1  10
+#define LightSensor2  4
+#define LightSensor3  2
+
+class EBot {
+public:
+  EBot();
+  ~EBot();
+
+  enum line { OK, RIGHT, LEFT, NOK};
+
+  void move(bool speed);
+  void move(int speed);
+  void stop();
+  void forward();
+  void backward();
+  void turnLeft();
+  void turnRight();
+  float distance();
+  void servoRotate(int angle);
+  line followLine();
+
+private:
+
+};
+
+#endif
