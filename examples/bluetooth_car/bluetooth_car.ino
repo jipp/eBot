0ae4dd3baf14e7e7ac2ec4638cdc77b50ec7a06c
jipp <wolfgang.keller@wobilix.de>
2017-04-12 09:58:43 +0200

    modified for new lib

--- examples/bluetooth_car/bluetooth_car.ino
+++ examples/bluetooth_car/bluetooth_car.ino
@@ -1,108 +1,41 @@
-//www.elegoo.com
-//2016.09.19
+#include "Arduino.h"
 
-int LED=13;
-volatile int state = LOW;
+#include <EBot.h>
+
+EBot eBot = EBot();
+
+bool state = LOW;
 char getstr;
-int in1=6;
-int in2=7;
-int in3=8;
-int in4=9;
-int ENA=5;
-int ENB=11;
 int ABS=150;
-void _mForward()
-{ 
-  digitalWrite(ENA,HIGH);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(in1,HIGH);//digital output
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,HIGH);
-  Serial.println("Forward");
-}
-void _mBack()
-{
-  digitalWrite(ENA,HIGH);
-  digitalWrite(ENB,HIGH);
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,HIGH);
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW);
-  Serial.println("Back");
-}
-void _mleft()
-{
-  analogWrite(ENA,ABS);
-  analogWrite(ENB,ABS);
-  digitalWrite(in1,HIGH);
-  digitalWrite(in2,LOW);
-  digitalWrite(in3,HIGH);
-  digitalWrite(in4,LOW); 
-  Serial.println("go left!");
-}
-void _mright()
-{
-  analogWrite(ENA,ABS);
-  analogWrite(ENB,ABS);
-  digitalWrite(in1,LOW);
-  digitalWrite(in2,HIGH);
-  digitalWrite(in3,LOW);
-  digitalWrite(in4,HIGH);
-  Serial.println("go right!");
-}
-void _mStop()
-{
-  digitalWrite(ENA,LOW);
-  digitalWrite(ENB,LOW);
-  Serial.println("Stop!");
-}
-void stateChange()
-{
+
+void stateChange() {
   state = !state;
-  digitalWrite(LED, state);  
-}
-void setup()
-{ 
-  pinMode(LED, OUTPUT);
-  Serial.begin(9600);
-  pinMode(in1,OUTPUT);
-  pinMode(in2,OUTPUT);
-  pinMode(in3,OUTPUT);
-  pinMode(in4,OUTPUT);
-  pinMode(ENA,OUTPUT);
-  pinMode(ENB,OUTPUT);
-  _mStop();
-}
-void loop()
-  { 
-  getstr=Serial.read();
- if(getstr=='f')
-  {
-    _mForward();
-  }
-  else if(getstr=='b')
-  {
-    _mBack();
-    delay(200);
-  }
-  else if(getstr=='l')
-  {
-    _mleft();
-    delay(200);
-  }
-  else if(getstr=='r')
-  {
-    _mright();
-    delay(200);
-  }
-  else if(getstr=='s')
-  {
-     _mStop();     
-  }
-  else if(getstr=='A')
-  {
-  stateChange();
-  }
+  digitalWrite(LED_BUILTIN, state);
 }
 
+void setup(){
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
+  eBot.move(ABS);
+  eBot.stop();
+}
+
+void loop() {
+  getstr=Serial.read();
+  if (getstr=='f') {
+    eBot.forward();
+  } else if (getstr=='b') {
+    eBot.backward();
+    delay(200);
+  } else if (getstr=='l') {
+    eBot.turnLeft();
+    delay(200);
+  } else if (getstr=='r') {
+    eBot.turnRight();
+    delay(200);
+  } else if (getstr=='s') {
+    eBot.stop();
+  } else if (getstr=='A') {
+    stateChange();
+  }
+}
