f3230b7e02b5e1491b77c7225a753b912f09ea61
jipp <wolfgang.keller@wobilix.de>
2017-04-12 08:40:21 +0200

    bluetooth_blink updated

--- examples/bluetooth_blink/bluetooth_blink.ino
+++ examples/bluetooth_blink/bluetooth_blink.ino
@@ -1,22 +1,25 @@
-//www.elegoo.com
-//2016.09.19
+#include "Arduino.h"
 
-int LED=13;//Define 13 pin for LED
-volatile int state = LOW;//The initial state of the function is defined as a low level
-char getstr;    //Defines a function that receives the Bluetooth character
+#include <EBot.h>
+
+EBot eBot = EBot();
+
+bool state = LOW;
+char getstr;
+
 void setup() {
- pinMode(LED, OUTPUT);
-Serial.begin(9600);
+  pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(115200);
 }
-/*Control LED sub function*/
-void stateChange()
-{
+
+void stateChange() {
   state = !state;
-  digitalWrite(LED, state);  
+  digitalWrite(LED_BUILTIN, state);
 }
+
 void loop() {
-    getstr=Serial.read();//The Bluetooth serial port to receive the data in the function
-    if(getstr=='A'){
+  getstr=Serial.read();//The Bluetooth serial port to receive the data in the function
+  if(getstr=='A'){
     stateChange();
   }
 }
