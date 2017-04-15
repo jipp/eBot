6a54e85f5198cf85bbe447c1bb27e1995065f6ca
jipp <wolfgang.keller@wobilix.de>
2017-04-15 21:14:09 +0200

    added Lesson 5

---
+++ examples/Lesson 5/new_Line_tracking_car/new_Line_tracking_car.ino
@@ -0,0 +1,46 @@
+#include "Arduino.h"
+#include <EBot.h>
+
+int speed = 115;
+
+EBot eBot = EBot();
+
+void setup() {
+  eBot.begin();
+}
+
+void loop() {
+  int num1,num2,num3;
+  num1=digitalRead(10);
+  num2=digitalRead(4);
+  num3=digitalRead(2);
+  if ((num1==0) && num2 && num3) {
+    eBot.rotateLeft();          //The sensor detected that right car turn left immediately when it meets black line
+    delay(2);
+    while(1) {
+      num2=digitalRead(2);         //Cycle to judge degree of intermediate sensor,
+      if (num2==1) {
+        eBot.rotateLeft();                     //If num2==1 does not go to the middle position, continue to turn left.
+        delay(2);
+      } else {
+        break;                      //Detection of num2==0 instructions turned over, out of the loop, detection of three sensors’ statusand then make appropriate action
+      }
+    }                                       //The following and so on
+  } else if (num2 && num1 && (num3==0)) {
+    eBot.rotateRight();
+    delay(2);
+    while(1) {
+      num2=digitalRead(2);
+      if (num2==1) {
+        eBot.rotateRight();
+        delay(2);
+      } else {
+        break;
+      }
+    }
+  }
+  else {
+    eBot.forward();
+    delay(2);
+  }
+}
