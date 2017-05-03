082c10d96c2bcf64d574f224ead9e81c1f15d9d3
jipp <wolfgang.keller@wobilix.de>
2017-05-03 16:35:45 +0200

    at least it compiles

--- examples/Lesson3/new_Infrared_remote_control_car/new_Infrared_remote_control_car.ino
+++ examples/Lesson3/new_Infrared_remote_control_car/new_Infrared_remote_control_car.ino
@@ -1,5 +1,5 @@
 #include "Arduino.h"
-#define WOKE
+#include <IRremote.h>
 #include <EBot.h>
 
 #define A 16736925
@@ -8,35 +8,44 @@
 #define C 16720605
 #define D 16761405
 
-int ABS=150;
+int receiverpin = 12;
+int speed=150;
 unsigned long RED;
+IRrecv irrecv(receiverpin);
+decode_results results;
 
 EBot eBot = EBot();
 
 void setup() {
   pinMode(LED_BUILTIN, OUTPUT);
   Serial.begin(9600);
+  irrecv.enableIRIn();
   eBot.begin();
   eBot.setSpeed(speed);
   eBot.setDirection();
 }
 
 void loop() {
-  RED = eBot.getIR();
-  if(RED==A) {
-    Serial.println("A");
-    eBot.setDirection(EBot::FORWARD);
-  } else if(RED==B) {
-    Serial.println("B");
-    eBot.setDirection(EBot::BACKWARD);
-  } else if(RED==C) {
-    Serial.println("C");
-    eBot.setDirection(EBot::ROTATELEFT);
-  } else if(RED==D) {
-    Serial.println("D");
-    eBot.setDirection(EBot::ROTATERIGHT);
-  } else if(RED==X) {
-    Serial.println("X");
-    eBot.setDirection();
+  if (irrecv.decode(&results)) {
+    RED=results.value;
+    Serial.println(RED);
+    irrecv.resume();
+    delay(150);
+    if(RED==A) {
+      Serial.println("A");
+      eBot.setDirection(EBot::FORWARD);
+    } else if(RED==B) {
+      Serial.println("B");
+      eBot.setDirection(EBot::BACKWARD);
+    } else if(RED==C) {
+      Serial.println("C");
+      eBot.setDirection(EBot::ROTATELEFT);
+    } else if(RED==D) {
+      Serial.println("D");
+      eBot.setDirection(EBot::ROTATERIGHT);
+    } else if(RED==X) {
+      Serial.println("X");
+      eBot.setDirection();
+    }
   }
 }
